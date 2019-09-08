90f9c49333cf3f8bc1a2a4e09395cd0903558f4b
denis-onder <denis_onder@protonmail.com>
2019-09-08 15:02:26 +0200

    Added empty string fallbacks to the config

--- services/auth/src/config/index.ts
+++ services/auth/src/config/index.ts
@@ -9,9 +9,9 @@ const config: IConfigInterface = {
     port: parseInt(process.env.SERVER_PORT) || 8001
   },
   database: {
-    uri: process.env.DB_URI,
+    uri: process.env.DB_URI || "",
     port: parseInt(process.env.DB_PORT) || 27017,
-    name: process.env.DB_NAME
+    name: process.env.DB_NAME || "tythus"
   },
   environment: process.env.NODE_ENV || "development",
   secretOrKey: process.env.SECRET_OR_KEY || "secret"
